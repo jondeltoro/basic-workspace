56d0c0eb3690e5beca1f9ace5c0f40e921837ccf
Jonathan Del Toro <jonathandeltoro@outlook.com>
2020-03-16 01:34:23 -0700

    ngrx store

--- libs/profile-store/profile.effects.ts
+++ libs/profile-store/profile.effects.ts
@@ -1,17 +1,78 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import { Store, select } from '@ngrx/store';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import {
+  switchMap,
+  map,
+  catchError,
+  withLatestFrom,
+  concatMap,
+  tap
+} from 'rxjs/operators';
+
+import * as profileActions from './profile.actions';
+import * as fromProfile from './profile.reducers';
+import { ProfileService } from './profile.service';
+import {
+  UserProfile,
+  ProfileState
+} from '@monofunworkspace/feature-profile-details';
 
 @Injectable()
 export class ProfileEffects {
+  getRandomUserProfileRequest$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(profileActions.getRandomProfile),
+      switchMap(() => {
+        const random = Math.floor(
+          Math.random() * this.profileService.maxResults
+        );
+        return this.handleGetUserProfile(random);
+      })
+    )
+  );
 
-  getUserProfileRequest$ = createEffect(() => this.actions$.pipe(
-    // Write code to retrieve random user from API here
-    // Hint: You will need to use some rxjs operators here
-  ));
+  getUserProfileRequest$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(profileActions.getProfile),
+      concatMap(action =>
+        of(action).pipe(
+          withLatestFrom(
+            this.store.pipe(
+              select(fromProfile.profileFeatureKey),
+              map((res: ProfileState) => res)
+            )
+          )
+        )
+      ),
+      switchMap(([{ id }, { userProfileList }]) => {
+        // check if profile is already in the store (userProfileList)
+        if (userProfileList && userProfileList[id]) {
+          profileActions.getProfileSuccess({
+            profile: userProfileList[id]
+          });
+        } else {
+          // if not in the store then get it using service
+          return this.handleGetUserProfile(id);
+        }
+      })
+    )
+  );
+
+  private handleGetUserProfile(id: number) {
+    return this.profileService.getUserProfiles(id).pipe(
+      map((users: UserProfile[]) =>
+        profileActions.getProfileSuccess({ profile: users[0] })
+      ),
+      catchError(error => of(profileActions.getProfileError(error)))
+    );
+  }
 
   constructor(
-    private actions$: Actions
+    private actions$: Actions,
+    private store: Store<any>,
+    private profileService: ProfileService
   ) {}
 }
-
-// FYI: The response from the API will return an object with different properties than the UserProfile model.
+//VwithLatestFrom
--- libs/profile-store/profile.reducers.ts
+++ libs/profile-store/profile.reducers.ts
@@ -1,19 +1,28 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import * as ProfileActions from './profile.actions';
 import { ProfileState } from './../feature-profile-details/src/lib/models/profile-state.model';
+// import { UserProfile } from 'libs/feature-profile-details/src/lib/models';
+
+export const profileFeatureKey = 'profile';
 
 export const initialState: ProfileState = {
-    userProfile: null
+  userProfile: null,
+  userProfileList: null
+};
+
+const profileReducer = createReducer(
+  initialState,
+  on(ProfileActions.getProfileSuccess, (state, { profile }) => {
+    return { ...state, userProfile: profile };
+  }),
+  on(ProfileActions.getProfileError, state => {
+    return { ...state, userProfile: initialState.userProfile };
+  }),
+  on(ProfileActions.resetProfile, state => {
+    return { ...state, userProfile: initialState.userProfile };
+  })
+);
+
+export function reducer(state: ProfileState | undefined, action: Action) {
+  return profileReducer(state, action);
 }
-
-const scoreboardReducer = createReducer(
-    initialState,
-    on(ProfileActions.getProfile, state => { 
-        // Write code here
-        return { ...state }
-    })
-  );
-
-  export function reducer(state: ProfileState | undefined, action: Action) {
-    return scoreboardReducer(state, action);
-  }
