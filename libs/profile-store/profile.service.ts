3ef151cf9c8e96fa5bd758d1a4ff231840e3aa74
Jonathan Del Toro <jonathandeltoro@outlook.com>
2020-03-16 01:33:33 -0700

    Profile service xhr

--- libs/profile-store/profile.service.ts
+++ libs/profile-store/profile.service.ts
@@ -2,13 +2,51 @@
 // Further documentation for implementation can be found here: https://randomuser.me/documentation#format
 
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { UserProfile } from 'libs/feature-profile-details/src/lib/models';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-export class FormsService {
+export class ProfileService {
+  constructor(private http: HttpClient) {}
 
-    getUserProfile() {
-        // Write code here to retrieve a user profile from the random user API
+  readonly maxResults = 15;
+
+  private params: { [key: string]: string } = {
+    inc: 'name,location,email,phone,cell,picture',
+    results: String(this.maxResults),
+    nat: 'US',
+    seed: 'scorpion'
+  };
+
+  normalizeRecord(rec: any, index: number): UserProfile {
+    return {
+      id: index,
+      firstName: rec.name.first,
+      lastName: rec.name.last,
+      city: rec.location.city,
+      state: rec.location.state,
+      email: rec.email,
+      phone: rec.phone,
+      cell: rec.cell,
+      pictureUrl: rec.picture.thumbnail
+    };
+  }
+
+  getUserProfiles(id = null) {
+    const params = { ...this.params };
+    if (id !== null) {
+      params.page = String(id);
+      params.results = '1';
     }
+    return this.http
+      .get('/api/profiles', { params })
+      .pipe(
+        map(({ results }: { results: any[] }) =>
+          results.map(this.normalizeRecord)
+        )
+      );
+  }
 }
